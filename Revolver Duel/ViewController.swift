7a4200d8688a7029672013d53b4487e7e4c26558
Albin Martinsson <albinmartinsson91@gmail.com>
2016-08-02 14:16:19 +0200

    Sync

--- Revolver Duel/ViewController.swift
+++ Revolver Duel/ViewController.swift
@@ -15,12 +15,20 @@ let motionKit = MotionKit()
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var smokeView: UIView!
-    let particleEmitter = CAEmitterLayer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.createParticles()
+        /* Setup your scene here */
+        
+        var path = NSBundle.mainBundle().pathForResource("SmokeParticle", ofType: "sks")
+        var rainParticle = NSKeyedUnarchiver.unarchiveObjectWithFile(path!) as! SKEmitterNode
+        
+        rainParticle.position = CGPointMake(self.size.width/2, self.size.height)
+        rainParticle.name = "rainParticle"
+        rainParticle.targetNode = self.scene
+        
+        self.addChild(rainParticle)
         
         motionKit.getAccelerometerValues(0.1) { (x, y, z) -> () in
             if(x > 0.85) {
@@ -39,36 +47,6 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    func createParticles() {
-        particleEmitter.emitterPosition = CGPoint(x: view.center.x, y: -96)
-        particleEmitter.emitterShape = kCAEmitterLayerLine
-        particleEmitter.emitterSize = CGSize(width: view.frame.size.width, height: 1)
-        
-        let white = makeEmitterCellWithColor(UIColor(red:0.00, green:0.00, blue:0.00, alpha:0.3))
-        smokeView.layer.addSublayer(particleEmitter)
-        
-    }
-    
-    func makeEmitterCellWithColor(color: UIColor) -> CAEmitterCell {
-        let cell = CAEmitterCell()
-        cell.birthRate = 3
-        cell.lifetime = 20.0
-        cell.lifetimeRange = 0
-        cell.color = UIColor(red:0.00, green:0.00, blue:0.00, alpha:0.5).CGColor
-        cell.velocity = 30
-        cell.velocityRange = 25
-        cell.alphaSpeed =  -1.0/cell.lifetime
-        cell.emissionLongitude = CGFloat(M_PI)
-        cell.emissionRange = CGFloat(M_PI_4)
-        cell.scale = 0.25
-        cell.scaleRange = 0.25
-        cell.scaleSpeed = -0.05
-        cell.contents = UIImage(named: "spark")?.CGImage
-        
-        return cell
-    }
-    
 
 
     override func didReceiveMemoryWarning() {
